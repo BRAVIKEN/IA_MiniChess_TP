25fb8717f86ca5b4e8378ed0b55aebe3ef41233a
TheMagnat <guillaume.magniadas@gmail.com>
2020-03-05 23:08:04 +0100

    Added toDo checkMate

--- GameHelper.hpp
+++ GameHelper.hpp
@@ -7,16 +7,18 @@
 
 class GameHelper {
 
-	static bool checkBlack(int x, int y, chess_board_t const& board);
+	static bool checkBlack(int kingX, int kingY, chess_board_t const& board);
 	
-	static bool checkWhite(int x, int y, chess_board_t const& board);
+	static bool checkWhite(int kingX, int kingY, chess_board_t const& board);
 
+	//TODO
+	static bool checkMateBlack(int kingX, int kingY, chess_board_t const& board);
+	static bool checkMateWhite(int kingX, int kingY, chess_board_t const& board);
 
 	///TO DO
-
 	//Fonction qui renvoie tout les coups possible de blanc et noir
-	static std::vector<chess_move_t> AllPossibleMovesWhite();
+	static std::vector<chess_move_t> AllPossibleMovesWhite(chess_board_t const& board);
 
-	static std::vector<chess_move_t> AllPossibleMovesBlack();
+	static std::vector<chess_move_t> AllPossibleMovesBlack(chess_board_t const& board);
 
 };
