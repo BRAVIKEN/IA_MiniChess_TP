217f44b18e34d80a95f096d10cac8b7d3f27c9c7
TheMagnat <guillaume.magniadas@gmail.com>
2020-03-05 23:24:41 +0100

    added score to do.

--- GameHelper.hpp
+++ GameHelper.hpp
@@ -8,7 +8,6 @@
 class GameHelper {
 
 	static bool checkBlack(int kingX, int kingY, chess_board_t const& board);
-	
 	static bool checkWhite(int kingX, int kingY, chess_board_t const& board);
 
 	//TODO
@@ -18,7 +17,10 @@ class GameHelper {
 	///TO DO
 	//Fonction qui renvoie tout les coups possible de blanc et noir
 	static std::vector<chess_move_t> AllPossibleMovesWhite(chess_board_t const& board);
-
 	static std::vector<chess_move_t> AllPossibleMovesBlack(chess_board_t const& board);
+
+	///TODO
+	static int whiteScore(chess_board_t const& board);
+	static int blackScore(chess_board_t const& board);
 
 };
