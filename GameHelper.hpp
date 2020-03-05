c1703b9848a12f019ccfce60d83b1c841fdd3ef1
TheMagnat <guillaume.magniadas@gmail.com>
2020-03-05 23:03:37 +0100

    forgot static keyword

--- GameHelper.hpp
+++ GameHelper.hpp
@@ -7,16 +7,16 @@
 
 class GameHelper {
 
-	bool checkBlack(int x, int y, chess_board_t const& board);
+	static bool checkBlack(int x, int y, chess_board_t const& board);
 	
-	bool checkWhite(int x, int y, chess_board_t const& board);
+	static bool checkWhite(int x, int y, chess_board_t const& board);
 
 
 	///TO DO
 
 	//Fonction qui renvoie tout les coups possible de blanc et noir
-	std::vector<chess_move_t> AllPossibleMovesWhite();
+	static std::vector<chess_move_t> AllPossibleMovesWhite();
 
-	std::vector<chess_move_t> AllPossibleMovesBlack();
+	static std::vector<chess_move_t> AllPossibleMovesBlack();
 
 };
